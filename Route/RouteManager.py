4b3ca5515a5ca3e7ac9e72cee9672ffe567a94a0
stories2 <stories2@naver.com>
2017-05-19 00:30:43 +0000

    Made client -> server send message api

--- Route/RouteManager.py
+++ Route/RouteManager.py
@@ -1,5 +1,12 @@
 from . import routes
+from flask import request
 
 @routes.route("/")
-def hello():
+def Hello():
 	return "Hello World!"
+
+@routes.route("/messenger/v1.0.0/send/<uploaderName>", methods=['POST'])
+def MessageSend(uploaderName):
+	print uploaderName, ": ", request.data
+	return "Ok"
+	
