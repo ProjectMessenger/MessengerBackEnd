2d7b2c5607b323f7bd76e8e382965b1e55bcbc23
stories2 <stories2@naver.com>
2017-05-19 09:26:16 +0000

    Tab makes me mad

--- Utils/Logger.py
+++ Utils/Logger.py
@@ -3,21 +3,19 @@ from Setting import DefineManager
 
 def PrintLogMessage(fileName = "Logger", methodName = "PrintLogMessage", message = "Wrong log level accepted.", logLevel = DefineManager.LOG_LEVEL_ERROR):
     logger = logging.getLogger()
-	logMessage = "[" + fileName + "] <" + methodName + "> (" + message + ")"
-	if logLevel == DefineManager.LOG_LEVEL_INFO:
-		logger.setLevel(logging.INFO)
-		logging.info(logMessage)
-	elif logLevel == DefineManager.LOG_LEVEL_DEBUG:
+    logMessage = "[" + fileName + "] <" + methodName + "> (" + message + ")"
+    if logLevel == DefineManager.LOG_LEVEL_INFO:
+        logger.setLevel(logging.INFO)
+        logging.info(logMessage)
+    elif logLevel == DefineManager.LOG_LEVEL_DEBUG:
 		logger.setLevel(logging.DEBUG)
-		LogLevelDebug(logMessage)
 		logging.debug(logMessage)
-	elif logLevel == DefineManager.LOG_LEVEL_WARN:
+    elif logLevel == DefineManager.LOG_LEVEL_WARN:
 		logger.setLevel(logging.WARN)
-		LogLevelWarn(logMessage)
 		logging.warning(logMessage)
-	elif logLevel == DefineManager.LOG_LEVEL_ERROR:
+    elif logLevel == DefineManager.LOG_LEVEL_ERROR:
 		logger.setLevel(logging.ERROR)
 		logging.error(logMessage)
-	else:
+    else:
 		logger.setLevel(logging.ERROR)
-	return
+    return
